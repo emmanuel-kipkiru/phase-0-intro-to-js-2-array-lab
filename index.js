d4e5609093cd089d01a865e556519c815be6e251
emmanuel-kipkiru <emmanuelkipkirui17@gmail.com>
2022-06-13 19:58:58 +0300

    Done.

--- index.js
+++ index.js
@@ -12,16 +12,10 @@ function destructivelyRemoveFirstCat (){
     cats.shift()
 }
 function appendCat (name){
-    return {
-        cats,
-        name,
-    }
+    return [...cats, name];
 }
 function prependCat (name){
-    return {
-        name,
-        cats,
-    }
+    return [name, ...cats];
 }
 function removeLastCat (){
     return cats.slice(0, 2)
